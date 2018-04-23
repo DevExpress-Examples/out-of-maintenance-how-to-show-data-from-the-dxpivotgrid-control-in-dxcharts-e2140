671ff696dbbbccfd02749ed23f6d20c2d99ac584
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ShowDatafromtheDXPivotGridControlinDXCharts/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Data.OleDb;
+using System.Windows;
+using DevExpress.Xpf.PivotGrid;
+using HowToBindToMDB.NwindDataSetTableAdapters;
+using DevExpress.XtraPivotGrid;
+
+namespace HowToBindToMDB {
+    public partial class MainWindow : Window {
+        NwindDataSet.SalesPersonDataTable salesPersonDataTable = 
+            new NwindDataSet.SalesPersonDataTable();
+
+        SalesPersonTableAdapter salesPersonDataAdapter = 
+            new SalesPersonTableAdapter();
+
+        int minSum = 500;
+
+        public MainWindow() {
+            InitializeComponent();
+            pivotGridControl1.DataSource = salesPersonDataTable;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            salesPersonDataAdapter.Fill(salesPersonDataTable);
+        }
+    }
+}

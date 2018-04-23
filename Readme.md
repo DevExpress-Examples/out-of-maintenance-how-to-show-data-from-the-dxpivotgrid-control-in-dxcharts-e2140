f00b84ba90bd8e46a5d7776e63ba101f9dd281e1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Show Data from the DXPivotGrid Control in DXCharts
+
+
+<p>This example shows how to visualize data displayed by the DXPivotGrid using the DXCharts Suite.</p>
+
+<br/>
+
+
---
+++ VB/ShowDatafromtheDXPivotGridControlinDXCharts/MainWindow.xaml.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.OleDb
+Imports System.Windows
+Imports DevExpress.Xpf.PivotGrid
+Imports nwindDataSetTableAdapters
+Imports DevExpress.XtraPivotGrid
+
+Namespace HowToBindToMDB
+	Partial Public Class MainWindow
+		Inherits Window
+		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
+		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
+		Private minSum As Integer = 500
+
+		Public Sub New()
+			InitializeComponent()
+			pivotGridControl1.DataSource = salesPersonDataTable
+		End Sub
+
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			salesPersonDataAdapter.Fill(salesPersonDataTable)
+		End Sub
+	End Class
+End Namespace

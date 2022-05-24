6ddbe2c6bbfa088b5fa4d1e8d09e22f77a7f4bc5
DevExpressExampleBot <fakeEmail>
2022-05-24 12:25:41 +0400

    Source auto update [skip ci]

--- VB/ShowDatafromtheDXPivotGridControlinDXCharts/MainWindow.xaml.vb
+++ VB/ShowDatafromtheDXPivotGridControlinDXCharts/MainWindow.xaml.vb
@@ -1,25 +1,24 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
-Imports System.Data.OleDb
 Imports System.Windows
-Imports DevExpress.Xpf.PivotGrid
-Imports nwindDataSetTableAdapters
-Imports DevExpress.XtraPivotGrid
+Imports HowToBindToMDB.NwindDataSetTableAdapters
 
 Namespace HowToBindToMDB
-	Partial Public Class MainWindow
-		Inherits Window
-		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
-		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
-		Private minSum As Integer = 500
 
-		Public Sub New()
-			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataTable
-		End Sub
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			salesPersonDataAdapter.Fill(salesPersonDataTable)
-		End Sub
-	End Class
+        Private salesPersonDataTable As NwindDataSet.SalesPersonDataTable = New NwindDataSet.SalesPersonDataTable()
+
+        Private salesPersonDataAdapter As SalesPersonTableAdapter = New SalesPersonTableAdapter()
+
+        Private minSum As Integer = 500
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.pivotGridControl1.DataSource = salesPersonDataTable
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            salesPersonDataAdapter.Fill(salesPersonDataTable)
+        End Sub
+    End Class
 End Namespace
